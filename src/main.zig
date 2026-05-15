17bca534579bf1b59aa3523307f101e31029b865
lp <lilko.petkovv@gmail.com>
2026-05-15 11:37:54 +0300

    refactor: refactor code to for module split

--- src/main.zig
+++ src/main.zig
@@ -3,40 +3,15 @@ const docer = @import("docer");
 const print = std.debug.print;
 const Allocator = std.mem.Allocator;
 
+const s = @import("schemas/schemas.zig");
+const pfp = @import("file_processors/python_file_processor.zig");
+
+const t = @import("file_processors/test_python_file_processor.zig");
+test {
+    std.testing.refAllDecls(t);
+}
+
 const TARGET_DIRECTORY: []const u8 = "/home/lpetkov/Tasks/zig_tasks/docer/python_test";
-
-const FileTypes = enum {
-    py,
-    go,
-    zig,
-};
-
-const PythonObjectContext = struct {
-    func_found: bool = false,
-    closing_func_bracket_found: bool = false,
-    func_recorded: bool = false,
-    doc_string_found: bool = false,
-};
-
-const File = struct {
-    file_type: FileTypes, // py / go / zig
-    fd: std.fs.File,
-    target_file: ?std.fs.File,
-    file_size: u64,
-};
-
-const PythonFuncAndDoc = struct {
-    func: ?[]u8,
-    docstring: ?[]u8,
-};
-
-const PythonData = struct {
-    data: std.ArrayList(PythonFuncAndDoc),
-};
-
-const SPAWN_CONFIG: std.Thread.SpawnConfig = .{
-    .stack_size = 1024 * 16,
-};
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
@@ -58,14 +33,13 @@ pub fn main() !void {
         const fd: std.fs.File = try dir.openFile(file.path, .{});
 
         if (std.ascii.endsWithIgnoreCase(file_name, ".py")) {
-            var f: File = .{
-                .file_type = .py,
+            var f: s.File = .{
                 .fd = fd,
                 .file_size = file_size,
                 .target_file = target_file,
             };
 
-            var python_data = try processFile(allocator, &f);
+            var python_data = try pfp.processFile(allocator, &f);
             for (python_data.items) |item| {
                 if (item.func != null) {
                     print("{s}\n", .{item.func.?});
@@ -78,8 +52,6 @@ pub fn main() !void {
             }
 
             python_data.deinit(allocator);
-            // const thread = try std.Thread.spawn(SPAWN_CONFIG, processFile, .{ allocator, &f });
-            // thread.join();
         } else if (std.ascii.endsWithIgnoreCase(file_name, ".go")) {
             print("It is a go file\n", .{});
         } else if (std.ascii.endsWithIgnoreCase(file_name, ".zig")) {
@@ -87,258 +59,5 @@ pub fn main() !void {
         }
 
         fd.close();
-    }
-}
-
-fn processFile(allocator: Allocator, file: *File) !std.ArrayList(PythonFuncAndDoc) {
-    const file_content_buf = try allocator.alloc(u8, file.file_size);
-    defer allocator.free(file_content_buf);
-    _ = try file.fd.read(file_content_buf);
-
-    var python_data: std.ArrayList(PythonFuncAndDoc) = try .initCapacity(allocator, 1024);
-    var func_data: std.ArrayList(u8) = try .initCapacity(allocator, 32);
-    defer func_data.deinit(allocator);
-    var func_doc_string: std.ArrayList(u8) = try .initCapacity(allocator, 256);
-    defer func_doc_string.deinit(allocator);
-
-    var context: PythonObjectContext = .{};
-
-    for (file_content_buf, 0..) |byte, idx| {
-        if (context.func_recorded) {
-            if (byte != '"' and
-                byte != ' ' and
-                byte != 0x0A and
-                !context.doc_string_found)
-            {
-                const func: []u8 = try allocator.dupe(u8, func_data.items);
-                func_data.clearAndFree(allocator);
-                const pfad: PythonFuncAndDoc = .{ .docstring = null, .func = func };
-                try python_data.append(allocator, pfad);
-
-                context.func_recorded = false;
-            } else if (byte == '"' and
-                idx + 2 < file_content_buf.len and
-                file_content_buf[idx + 1] == '"' and
-                file_content_buf[idx + 2] == '"')
-            {
-                if (!context.doc_string_found) {
-                    context.doc_string_found = true;
-                    try func_doc_string.append(allocator, byte);
-                } else {
-                    try func_doc_string.appendNTimes(allocator, 0x22, 3);
-
-                    const func: []u8 = try allocator.dupe(u8, func_data.items);
-                    const dc: []u8 = try allocator.dupe(u8, func_doc_string.items);
-
-                    func_data.clearAndFree(allocator);
-                    func_doc_string.clearAndFree(allocator);
-
-                    const pfad: PythonFuncAndDoc = .{ .docstring = dc, .func = func };
-                    try python_data.append(allocator, pfad);
-
-                    context.doc_string_found = false;
-                    context.func_recorded = false;
-                }
-            } else if (context.doc_string_found) {
-                try func_doc_string.append(allocator, byte);
-            }
-
-            continue;
-        }
-
-        if (context.func_found and byte == ')') {
-            try func_data.append(allocator, byte);
-            context.closing_func_bracket_found = true;
-        } else if (context.func_found and !context.closing_func_bracket_found) {
-            try func_data.append(allocator, byte);
-        } else if (context.func_found and context.closing_func_bracket_found) {
-            if (byte != ':') try func_data.append(allocator, byte) else {
-                context.func_found = false;
-                context.closing_func_bracket_found = false;
-                context.func_recorded = true;
-            }
-        }
-
-        if (byte == 'd' and
-            idx + 3 < file_content_buf.len and
-            file_content_buf[idx + 1] == 'e' and
-            file_content_buf[idx + 2] == 'f' and
-            file_content_buf[idx + 3] == ' ' and
-            !context.func_found)
-        {
-            try func_data.append(allocator, byte);
-            context.func_found = true;
-        }
-    }
-
-    return python_data;
-}
-
-const t = std.testing;
-// Tests
-test "test function with docstring" {
-    const ta = std.heap.page_allocator;
-
-    const file = try std.fs.cwd().openFile("python_test/test_single_function_ds.py", .{});
-    const file_size: usize = (try file.stat()).size;
-
-    var f: File = .{
-        .fd = file,
-        .file_size = file_size,
-        .file_type = .py,
-        .target_file = null,
-    };
-
-    var python_data = try processFile(ta, &f);
-    defer python_data.deinit(ta);
-    const expected_function = "def fib(x: int) -> int";
-    const expected_docstring =
-        \\"""
-        \\    This is a test docstring
-        \\    """
-    ;
-
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].func.?, expected_function));
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].docstring.?, expected_docstring));
-
-    ta.free(python_data.items[0].func.?);
-    ta.free(python_data.items[0].docstring.?);
-}
-
-test "test function without docstring" {
-    const ta = std.heap.page_allocator;
-
-    const file = try std.fs.cwd().openFile("python_test/test_single_function_no_ds.py", .{});
-    const file_size: usize = (try file.stat()).size;
-
-    var f: File = .{
-        .fd = file,
-        .file_size = file_size,
-        .file_type = .py,
-        .target_file = null,
-    };
-
-    var python_data = try processFile(ta, &f);
-    defer python_data.deinit(ta);
-    const expected_function = "def main() -> None";
-
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].func.?, expected_function));
-    ta.free(python_data.items[0].func.?);
-}
-
-test "test function with docstring as last entry" {
-    const ta = std.heap.page_allocator;
-
-    const file = try std.fs.cwd().openFile("python_test/test_single_function_last_entry_ds.py", .{});
-    const file_size: usize = (try file.stat()).size;
-
-    var f: File = .{
-        .fd = file,
-        .file_size = file_size,
-        .file_type = .py,
-        .target_file = null,
-    };
-
-    var python_data = try processFile(ta, &f);
-    defer python_data.deinit(ta);
-    const expected_function = "def main() -> None";
-    const expected_docstring =
-        \\"""
-        \\    This is a test docstring
-        \\    """
-    ;
-
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].func.?, expected_function));
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].docstring.?, expected_docstring));
-
-    ta.free(python_data.items[0].func.?);
-    ta.free(python_data.items[0].docstring.?);
-}
-
-test "test multiple functions with docstrings" {
-    const ta = std.heap.page_allocator;
-
-    const file = try std.fs.cwd().openFile("python_test/test_multiple_functions_with_doc_strings.py", .{});
-    const file_size: usize = (try file.stat()).size;
-
-    var f: File = .{
-        .fd = file,
-        .file_size = file_size,
-        .file_type = .py,
-        .target_file = null,
-    };
-
-    var python_data = try processFile(ta, &f);
-    defer python_data.deinit(ta);
-
-    const expected_function01 = "def test01() -> None";
-    const expected_docstring01 =
-        \\"""
-        \\    This is a test docstring
-        \\    """
-    ;
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].func.?, expected_function01));
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].docstring.?, expected_docstring01));
-
-    const expected_function02 = "def test02() -> None";
-    const expected_docstring02 =
-        \\"""
-        \\    This is a test docstring
-        \\    """
-    ;
-    if (python_data.items[1].func != null) try t.expect(std.mem.eql(u8, python_data.items[1].func.?, expected_function02));
-    if (python_data.items[1].func != null) try t.expect(std.mem.eql(u8, python_data.items[1].docstring.?, expected_docstring02));
-
-    const expected_function03 = "def test03()";
-    const expected_docstring03 =
-        \\"""
-        \\    This is a test docstring
-        \\    """
-    ;
-    if (python_data.items[2].func != null) try t.expect(std.mem.eql(u8, python_data.items[2].func.?, expected_function03));
-    if (python_data.items[2].func != null) try t.expect(std.mem.eql(u8, python_data.items[2].docstring.?, expected_docstring03));
-
-    for (python_data.items) |item| {
-        if (item.func != null) {
-            ta.free(item.func.?);
-        }
-        if (item.docstring != null) {
-            ta.free(item.docstring.?);
-        }
-    }
-}
-
-test "test multiple functions without docstrings" {
-    const ta = std.heap.page_allocator;
-
-    const file = try std.fs.cwd().openFile("python_test/test_multiple_functions_wihtout_docstrings.py", .{});
-    const file_size: usize = (try file.stat()).size;
-
-    var f: File = .{
-        .fd = file,
-        .file_size = file_size,
-        .file_type = .py,
-        .target_file = null,
-    };
-
-    var python_data = try processFile(ta, &f);
-    defer python_data.deinit(ta);
-
-    const expected_function01 = "def test01() -> None";
-    if (python_data.items[0].func != null) try t.expect(std.mem.eql(u8, python_data.items[0].func.?, expected_function01));
-
-    const expected_function02 = "def test02() -> None";
-    if (python_data.items[1].func != null) try t.expect(std.mem.eql(u8, python_data.items[1].func.?, expected_function02));
-
-    const expected_function03 = "def test03()";
-    if (python_data.items[2].func != null) try t.expect(std.mem.eql(u8, python_data.items[2].func.?, expected_function03));
-
-    for (python_data.items) |item| {
-        if (item.func != null) {
-            ta.free(item.func.?);
-        }
-        if (item.docstring != null) {
-            ta.free(item.docstring.?);
-        }
     }
 }
