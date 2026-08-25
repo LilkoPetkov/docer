6d481fb55ba779816c0b53b5e82e738949823053
Lilko Petkov <lilko.petkov@digits.schwarz>
2026-08-25 13:22:00 +0300

    chore: porting changes from the 0.16 refactor

--- Makefile
+++ Makefile
@@ -19,10 +19,11 @@ build-fast: ## Build with `ReleaseFast` flag
 	zig build -Doptimize=ReleaseFast
 
 clean: ## Clean all temporary zig resources
-	rm -r .zig-cache
-	rm -r zig-out
-	rm -r REPORT_*
-	rm -r .zig-cache
-	
+	- rm -r zig-pkg
+	- rm -r .zig-cache
+	- rm -r zig-out
+	- rm -r REPORT_*
+	- rm -r .zig-cache
+
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- src/args/args_processor.zig
+++ src/args/args_processor.zig
@@ -3,17 +3,14 @@ const Allocator = std.mem.Allocator;
 
 const print = std.debug.print;
 
-pub fn processArgs(allocator: Allocator) ![]const u8 {
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
+pub fn processArgs(allocator: Allocator, arg_iterator: *std.process.Args.Iterator) ![]const u8 {
+    if (arg_iterator.*.inner.remaining.len == 1) return error.MissingArguments;
 
-    _ = args.skip();
+    _ = arg_iterator.skip();
 
-    if (args.inner.count <= 1) return error.MissingArguments;
-
-    while (args.next()) |arg| {
+    while (arg_iterator.next()) |arg| {
         if (std.mem.eql(u8, arg, "-p") or std.mem.eql(u8, arg, "--path")) {
-            const arg_val = args.next();
+            const arg_val = arg_iterator.next();
 
             if (arg_val != null) {
                 const path_arg = try allocator.dupe(u8, arg_val.?);
